9a95318f71310f5c8f298278874755781c74ab02
dkirchhof <dkirchhof@gmail.com>
2022-01-29 14:55:56 +0100

    restructured folders

--- src/editor/editors/visualBlockEditor/index.tsx
+++ src/editor/editors/visualBlockEditor/index.tsx
@@ -3,9 +3,10 @@ import { useState } from "react";
 import { BlockConfigs, IBlock, IBlockConfig } from "../../../types/block";
 import { IPropEditorProps } from "../../types/propEditor";
 import { getParentPathAndIndex, getPath, getPathForChild, traversePath } from "../../utils/path";
+import { BlockEditor } from "./components/blockEditor";
+import { Outliner } from "./components/outliner";
+import { Preview } from "./components/preview";
 import { CMSContext } from "./contexts/cmsContext";
-import { Panel } from "./panel";
-import { Preview } from "./preview";
 import { Container } from "./styles";
 import { Selection } from "./types";
 import { createBlock } from "./utils/createBlock";
@@ -14,9 +15,9 @@ interface IOptions {
     blockConfigs: BlockConfigs;
 }
 
-
 export const visualBlockEditorFactory = (options: IOptions) => (props: IPropEditorProps<IBlock[]>) => {
     const [selection, setSelection] = useState<Selection>(null);
+    const selectedBlock = selection && traversePath(props.value, selection);
 
     const changeData = (path: string) => (prop: string) => (dataValue: any) => {
         if (path === "") {
@@ -70,7 +71,8 @@ export const visualBlockEditorFactory = (options: IOptions) => (props: IPropEdit
         <CMSContext.Provider value={{ selection, setSelection, blockConfigs: options.blockConfigs, addBlock, removeBlock }}>
             <Container>
                 <Preview blocks={props.value} ctx={{}} />
-                <Panel blocks={props.value} changeData={changeData} />
+                <Outliner blocks={props.value} />
+                {selectedBlock && <BlockEditor block={selectedBlock} onChange={changeData(selection)} />}
             </Container>
         </CMSContext.Provider>
     );
--- src/editor/editors/visualBlockEditor/styles.tsx
+++ src/editor/editors/visualBlockEditor/styles.tsx
@@ -2,9 +2,12 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     display: grid;
-    grid-template-columns: 1fr auto;
+    grid-template-columns: 1fr 256px 384px;
     gap: 16px;
 
     height: 100%;
     overflow: hidden;
+
+    /* scrollable container fix */
+    /* min-height: 0; */
 `;
