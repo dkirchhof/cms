cd13871dd7d7d9c0123382e9241ad2fa1bc400eb
dkirchhof <dkirchhof@gmail.com>
2022-03-26 10:30:20 +0100

    added typeof helper to locales

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,19 @@ import { createItemType, IItemType } from "./itemTypeBuilder";
 import { createEditorType } from "./itemTypeBuilder/editorField";
 import { createListType } from "./itemTypeBuilder/listField";
 import { requestHandlerFactory } from "./server";
+import { ILocales } from "./types/i18n";
 
-export const createItemTypeBuilder = <LOCALES extends string>(locales: readonly LOCALES[]) => ({
+export const createItemTypeBuilder = <LOCALES extends string>(locales: ILocales<LOCALES>) => ({
     createListType,
     createEditorType: createEditorType<LOCALES>(),
     createItemType: createItemType<LOCALES>(),
 });
 
-export const createCMS = (locales: readonly string[]) => {
+export const createCMS = <LOCALES extends string>(locales: ILocales<LOCALES>) => {
     return {
-        createEditor: (itemTypes: IItemType[]) => editorFactory(itemTypes, locales),
+        createEditor: (itemTypes: IItemType[]) => editorFactory(itemTypes, locales.locales),
         createAPI: (itemTypes: IItemType[]) => requestHandlerFactory(itemTypes),
+
+        getCurrentLocale: (context: { locale?: string; }) => context.locale as LOCALES, 
     };
 };
--- src/types/i18n.ts
+++ src/types/i18n.ts
@@ -1,5 +1,11 @@
 export type Localized<T, LOCALES extends readonly string[]> = Record<LOCALES[number], T>;
 
-export type ILocales<LOCALES extends string> = readonly LOCALES[];
+export interface ILocales<LOCALES extends string> {
+    locales: readonly LOCALES[];
+    t: LOCALES;
+}
 
-export const createLocales = <LOCALES extends string>(locales: readonly LOCALES[]): ILocales<LOCALES> => locales;
+export const createLocales = <LOCALES extends string>(locales: readonly LOCALES[]): ILocales<LOCALES> => ({
+    locales,
+    t: null as any,
+});
